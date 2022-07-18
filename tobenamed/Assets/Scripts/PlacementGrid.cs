9252bd152896b7d7074fc1cbe2a152a183626619
JamesPuli <70328459+JamesPuli@users.noreply.github.com>
2022-07-18 16:21:37 +1000

    Basic Grid System

---
+++ tobenamed/Assets/Scripts/PlacementGrid.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacementGrid : MonoBehaviour
+{
+    public PlacementGrid(Vector2Int gridSize) {
+        this.gridSize = gridSize;
+        tileArray = new GameObject[gridSize.x, gridSize.y];
+    }
+
+    public GameObject[,] tileArray;
+    public GameObject tilePrefab;
+    public Vector2Int gridSize;
+    public const float gridSpacing = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tileArray = new GameObject[gridSize.x, gridSize.y];
+        for (int x = 0; x < gridSize.x; x++) {
+            for(int y = 0; y < gridSize.y; y++) {
+                Vector3 spawnPosition = new Vector3(x, y) * gridSpacing;
+                Quaternion spawnRotation = new();
+                Transform parent = transform;
+                tileArray[x,y] = Instantiate(tilePrefab, spawnPosition, spawnRotation, parent);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
