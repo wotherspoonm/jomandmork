8de7c585e18794e79b1223c02bd7b72744f2b97f
JamesPuli <70328459+JamesPuli@users.noreply.github.com>
2022-07-26 00:40:01 +1000

    Adding Play button functionality

--- tobenamed/Assets/Scripts/UI/PlayButton.cs
+++ tobenamed/Assets/Scripts/UI/PlayButton.cs
@@ -1,8 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayButton : MonoBehaviour
 {
-    
+    private Button button;
+
+    private void Start() {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(delegate {
+            SceneManager.LoadScene("SampleScene");
+        });
+    }
 }
