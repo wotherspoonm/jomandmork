dd6547147ed485e1caeb5dfc039303713513393d
JamesPuli <70328459+JamesPuli@users.noreply.github.com>
2022-07-27 23:06:54 +1000

    InteractableObject abstract implementation

---
+++ tobenamed/Assets/Scripts/InteractableGameObject.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableGameObject : MonoBehaviour
+{
+    public Dictionary<KeyCode, Action> keycodeEventDictionary = new();
+    private void Awake() {
+        
+    }
+
+    private void OnMouseOver() {
+        foreach (KeyValuePair<KeyCode, Action> keyValuePair in keycodeEventDictionary) {
+            KeyCode keycode = keyValuePair.Key;
+            Action action = keyValuePair.Value;
+            if (Input.GetKeyDown(keycode)){
+                action();
+            }
+        }
+    }
+
+    public void AddListener(KeyCode keycode, Action action) {
+        keycodeEventDictionary.Add(keycode, action);
+    }
+}
